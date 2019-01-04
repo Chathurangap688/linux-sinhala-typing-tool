14b0801ce1d29993fbb48202b57aad1a296606df
RochaaP <rochpathiraja@gmail.com>
2019-01-04 12:56:33 +0530

    removed some errors

--- type.py
+++ type.py
@@ -5,21 +5,19 @@ from sys import exit
 def type_unicode(word,state,lenth):
     # print(state)
     
-    word.replace('d', 'p')
-    print word
    
-    # if(state):
-    #     print state
-    #     return
-    # for x in range(lenth+1):
-    #     pyautogui.press('backspace')
+    if(state):
+        print state
+        return
+    for x in range(lenth+1):
+        pyautogui.press('backspace')
 
     
-    # for x in range(0,len(word),5):
-    #     pyautogui.hotkey('ctrl', 'shift', 'u')
+    for x in range(0,len(word),5):
+        pyautogui.hotkey('ctrl', 'shift', 'u')
     
-    #     pyautogui.typewrite(word[x+1:x+5])
+        pyautogui.typewrite(word[x+1:x+5])
 
-    # pyautogui.press('space')
+    pyautogui.press('space')
     exit()
     
