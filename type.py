40f3e0b2fe2600fbfde17189df83036fc46cf1df
RochaaP <rochpathiraja@gmail.com>
2019-01-23 08:11:35 +0530

    GUI Implementation

--- type.py
+++ type.py
@@ -1,28 +1,34 @@
-
 from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-def typeUni(word,state,len):
-    if(word == False):
+
+
+def typeUni(word,state,len):            #arguments -  word - converted unicode values
+                                        #             state -  true or false
+                                        #             len -  user input word
+    if(word == False):                 
         state = False
         return 
+    
     word = word.replace('*','')
+
     if(state):
      
         len += 'd'
         for x in len:
-            #print(word)
-            keyboard.press(Key.backspace)
-            keyboard.release(Key.backspace)
-        #keyboard.press(Key.right)
-        #keyboard.release(Key.right)
-        #keyboard.type(' : ')
+            keyboard.press(Key.backspace)                  #erase the word, where user typed word by using backspace 
+            keyboard.release(Key.backspace)                #each presses there should be button release.
+        
+
+                                                           #the unicode value is converted to sinhala letters
+                                                           #with pressing shift + ctrl + unicode value
+                                                           #this process is done here
         with keyboard.pressed(Key.shift):
             with keyboard.pressed(Key.ctrl):
                 keyboard.type(word)
-        keyboard.type(' ')
+        keyboard.type(' ')                                 #after each word there should print a space
         
-        state = False
+        state = False                                      #make status false
     
-    return state
+    return state                                           #return status
