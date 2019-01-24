eacae0a7af7abe35659458534a30ff20b953cb86
Rochana Pathiraja <rochpathiraja@gmail.com>
2019-01-24 23:02:25 +0530

    Key Listener

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ import type
 import unicod
 import data_tree as data
 
-root = data.Tree('*')
+root = data.Tree('*')                   #initializing tree for data_tree
 root = unicod.add_unicode(root)
-
-state = False
-word = ""
+    
+state = False   
+word = ""                               #initializing word for store user input charactes
 def on_press(key):
     global root
     global state
     global word
-    if(state):
+    if(state):                              #if state == true, stop the process
         return
-    if (key == keyboard.Key.esc):
+    if (key == keyboard.Key.esc):           #if user presses esc button, stop the process
         # Stop listener
         return False
-    if (key == keyboard.Key.backspace):
+    if (key == keyboard.Key.backspace):     #if user presses backspace remove the last character from word
         word = word[:-1]
-    if(key ==  keyboard.Key.space):
+    if(key ==  keyboard.Key.space):         #if user presses space, the existing word valriable take as whole user input
         state = True
         temp_word = word
-        word = ""
-        uni_code = unicod.get_unicode(root,temp_word)
-        #print(uni_code)
-        state = type.typeUni(uni_code,state,temp_word)
-        state =False
-        word = ""
+        word = ""                                           #make the wprd variable free
+        uni_code = unicod.get_unicode(root,temp_word)       #pass the word to convert into sinhala unicode
+        
+        state = type.typeUni(uni_code,state,temp_word)      #pass the converted word to print
+        state =False                                        #make status as false
+                                                 
         
 
     try:
-        #print(format(key.char))
+        
         if(state):
             pass
         a=ord(key.char)
